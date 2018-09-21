1cce9815dd4d1306816b1b5f873f6b82a7ad1562
sq2141 <sam.qian4@gmail.com>
2018-09-20 22:33:56 -0400

    added folium map to webapp

--- webapp/app.py
+++ webapp/app.py
@@ -10,6 +10,11 @@ import numpy as np
 import psycopg2
 from dash.dependencies import Input, Output, State
 
+from matplotlib import pyplot as plt
+import seaborn as sns
+import folium
+from folium import plugins
+
 ### Read data from SQL
 
 # Python code to connect to Postgres
@@ -44,23 +49,46 @@ cosims_df = cosims_stacked_df.unstack()
 ###
 
 def generate_table(input_city, max_rows=10):
+    
     input_index = cities_df.index[cities_df['City']==input_city][0] # Get index of input
     input_sims = pd.DataFrame(cosims_df.iloc[input_index]) # Get sims for input city
     sims_sorted = input_sims.sort_values(by=input_index, ascending=False) # Sort sims
     sims_top10 = sims_sorted.iloc[1:11]
     top10_index = sims_top10.index.get_level_values(1)
     top10_cities = cities_df.iloc[top10_index][['City','Country','Lat','Lon']]
-    dataframe = top10_cities
+    top10_names = cities_df.iloc[top10_index][['City','Country']]
+    dataframe = top10_names    
     
-    out = html.Div([html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+    cities_map = folium.Map(location=[30, top10_cities['Lon'].mean()], zoom_start=2, 
+    tiles='Mapbox Bright', width=950, height=550)
+    top10_cities.apply(lambda row: folium.Marker(location=[row['Lat'], row['Lon']], popup = folium.Popup(row['City']+', '+row['Country'], parse_html=True)).add_to(cities_map), axis=1)
+    cities_map.save('cities_map.html')
+    
+    out = html.Div(
+        [html.Table(
+            [html.Tr(            
+            
+                # Left column
+                [html.Td(
+                    # Header Row
+                    [html.Tr([html.Th(col) for col in dataframe.columns])] +
 
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
-    )])
+                    # Body Row
+                    [html.Tr([
+                        html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                    ]) for i in range(min(len(dataframe), max_rows))]
+                )] +
+                
+                
+                # Right column
+                [html.Td(
+                    html.Iframe(id='map', srcDoc=open('cities_map.html','r').read(), width='290%', height='500')
+                )]                
+            )]
+             
+        
+        )]
+    )
     return out
 
 
@@ -72,7 +100,7 @@ app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
 
 app.layout = html.Div(children=[
     
-    html.H1(children='Cities you might be interested in',
+    html.H1(children='Suggested Cities',
            style = {'textAlign': 'center'}),
     
         
