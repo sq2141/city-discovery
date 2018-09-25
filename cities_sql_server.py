eadc4666658d6a9a0c1186335d1dc67dcccef977
sq2141 <sam.qian4@gmail.com>
2018-09-25 17:33:09 -0400

    replaced LDA with CountVectorizer

---
+++ cities_sql_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+import pandas as pd
+
+# Database and connection specifics
+username = 'postgres'
+password = 'samsam'
+host     = 'localhost'
+port     = '5432'
+db_name  = 'cities_db'
+
+# Create an engine, or a connection to a database
+engine = create_engine( 'postgresql://{}:{}@{}:{}/{}'.format(username, password, host, port, db_name) )
+print(engine.url)
+
+# Create the database if it doesn't exist already
+if not database_exists(engine.url):
+    create_database(engine.url)
+print(database_exists(engine.url))
+
+# Read .csv into pandas dataframe
+cities = pd.read_csv('cities_text_processed_df.csv', index_col = 0 )
+cosims_stacked = pd.read_csv('data/cos_sims_stacked.csv', index_col=[0,1], header = None)
+
+# Insert my data tables into SQL database
+cities.to_sql('cities_table', engine, if_exists='replace')
+cosims_stacked.to_sql('cosims_stacked_table', engine, if_exists='replace')
